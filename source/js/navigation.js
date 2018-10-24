ffede4f26574fdeb9ea22a5f38dedb5d426a8263
AlinaDenisova <denicov_vasya@mail.ru>
2018-10-24 14:04:26 +0300

    Верстка шапки

---
+++ source/js/navigation.js
@@ -0,0 +1,50 @@
+'use strict';
+
+window.navigation = (function () {
+  var MOBILE_WIDTH_MAX = 749;
+
+  var header = document.querySelector('.page-header');
+  var headerToggle = document.querySelector('.page-header__toggle');
+
+  var isHeaderOpened = function () {
+    return header.classList.contains('page-header--opened');
+  };
+
+  var isMobileScreen = function () {
+    return window.innerWidth <= MOBILE_WIDTH_MAX;
+  };
+
+  var showHeader = function () {
+    header.classList.remove('page-header--closed');
+    header.classList.add('page-header--opened');
+    window.addEventListener('resize', resizeWindowHandler);
+  };
+
+  var hideHeader = function () {
+    header.classList.remove('page-header--opened');
+    header.classList.add('page-header--closed');
+    window.removeEventListener('resize', resizeWindowHandler);
+  };
+
+  var onToggleClick = function () {
+    if (isHeaderOpened()) {
+      hideHeader();
+    } else {
+      showHeader();
+    }
+  };
+
+  var resizeWindowHandler = function () {
+    if (!isMobileScreen() && isHeaderOpened()) {
+      hideHeader();
+    }
+  };
+
+  headerToggle.addEventListener('click', onToggleClick);
+
+  return {
+    isOpened: isHeaderOpened,
+    show: showHeader,
+    hide: hideHeader
+  };
+})();
---
+++ source/js/remove-no-js.js
@@ -0,0 +1,6 @@
+'use strict';
+
+//удаление с body класса no-js
+(function () {
+  document.querySelector('.no-js').classList.remove('no-js');
+})();
